d55d9c66406a9fd5c6c1ea9e22d1c0d55cd80797
yuichi oyama <yuichi19991208@gmail.com>
2020-10-02 23:03:07 +0900

    他人の投稿を編集/削除できない様にした

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,6 @@ class FeedsController < ApplicationController
 
   def index
     @feeds = Feed.all.order(created_at: :desc)
-    
     if params[:back]
       @feed = Feed.new(feed_params)
     else
@@ -46,22 +45,26 @@ class FeedsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to @feed, notice: '更新しました' }
-        format.json { render :show, status: :ok, location: @feed }
-      else
-        format.html { render :edit }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+    if current_user == @feed.user
+      respond_to do |format|
+        if @feed.update(feed_params)
+          format.html { redirect_to @feed, notice: '更新しました' }
+          format.json { render :show, status: :ok, location: @feed }
+        else
+          format.html { render :edit }
+          format.json { render json: @feed.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   def destroy
-    @feed.destroy
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: '投稿を削除しました' }
-      format.json { head :no_content }
+    if current_user == @feed.user
+      @feed.destroy
+      respond_to do |format|
+        format.html { redirect_to feeds_url, notice: '投稿を削除しました' }
+        format.json { head :no_content }
+      end
     end
   end
 
